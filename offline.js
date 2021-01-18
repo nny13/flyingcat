72ff7abdf965fb8b1e6621734b63eb7bab3a7638
nny13 <28546@sts.ac.th>
2021-01-18 13:09:33 +0700

    Flyingcat

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1610949785,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/cat-sheet0.png",
+		"images/cat-sheet1.png",
+		"images/rockup-sheet0.png",
+		"images/rockdown-sheet0.png",
+		"images/itame-sheet0.png",
+		"images/itame-sheet1.png",
+		"images/itame-sheet2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
